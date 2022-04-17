1f696156af831c910dffb829ef3ba362396b3a98
maximombro <maximombro@gmail.com>
2022-04-17 17:38:14 -0400

    Added OpenFL code

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,14 @@
-package src;
+package ;
+
+import haxe.ui.HaxeUIApp;
 
 class Main {
-    static function main() {
-        Sys.println("Haxe is great!");
+    public static function main() {
+        var app = new HaxeUIApp();
+        app.ready(function() {
+            app.addComponent(new MainView());
+
+            app.start();
+        });
     }
 }
